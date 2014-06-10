fdbce4f727fa649e5cab6249ef071684ac3991e4
Serafeim Maroulis <seramak@gmail.com>
2014-06-10 18:47:33 +0100

    Code has been refactored

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,6 +1,5 @@
 require 'pry'
 
-$proceed = false
 class Board
 	def initialize
 		@board = [
@@ -9,32 +8,32 @@ class Board
 			%w(. . .)
 		]
 	end
-  # TODO - Add code so that the array at co-ordinate x, y is set to the value
-  # of marker, unless it has already been set.
-  #
-	def mark(x, y, marker)
-		if @board[x.to_i][y.to_i] == "." && @board[x.to_i][y.to_i]!= marker
-		   @board[x.to_i][y.to_i] = marker
-		   $proceed = true
-		else
-		   $proceed = false
-		end
+
+	def getBoard
+		@board
 	end
-	# TODO - Have the board return each of the possible winning combinations.
-	#
+
+	def mark(x, y, marker)
+		if @board[x][y] == "." && @board[x][y]!= marker
+			@board[x][y] = marker
+			mark = true
+		else
+			mark = false
+		end
+		mark
+	end
+
 	def each_winning_move
 		@board.each do |row|
 			yield row
 		end
-
 		@board.transpose.each do |col|
 			yield col
 		end
 		yield [@board[0][0], @board[1][1], @board[2][2]]
 		yield [@board[0][2], @board[1][1], @board[2][0]]
 	end
-	# TODO - Add code to return the board as a String, so that it appears
-	# in a 3 x 3 grid
+
 	def to_s
 		board = ""
 		@board.each do |x|
@@ -51,24 +50,45 @@ class Game
 		@players = [Nought, Cross]
 		@turn = @players.sample
 	end
-	# TODO - The main game loop goes in here.
-	#
+
 	def play
-		# While the game is still going on, do the following:
+	# While the game is still going on, do the following:
+		puts "Your game begins!"
+		# x = 0
+		# y = 0
+		for i in 1..9
 			# 1. Show the board to the user
+			puts @board
 			# 2. Prompt for an co-ordinate on the Board that we want to target
+			x = 0
+			y = 0
+			marked = user_choice do |coordinates|
 			# 3. Mark the board on the given square. If the input is invalid or already
 			# taken, go back to 1.
-			# 4. If we've got a winner, show the board and show a congratulations method.
-			# 5. Otherwise call next_turn and repeat.
-			# 6. How to detect a draw?
-			puts "Your game begins!"
-			draw
-			user_choice
+				x = coordinates[0].to_i
+				y = coordinates[1].to_i	
+				@board.mark(x,y,@turn.marker)
+			end
 
+			if !marked
+				redo
+			end
+			# 4. If we've got a winner, show the board and show a congratulations method.
+			if winner
+				puts @board
+				puts "#{winner} won!"
+				break
+			end
+			# 5. Otherwise call next_turn and repeat.
+			next_turn
+			# 6. How to detect a draw?
+			if i == 9
+				puts @board
+				puts "Draw!"
+			end
+		end		
 	end
-  # TODO - Return the next player's turn. I.e. not @turn but the other one.
-  #
+ 
 	def next_turn
 		if @turn == @players[0]
 			@turn = @players[1]
@@ -77,65 +97,26 @@ class Game
 		end
 	end
 
-	# TODO - Return the winning Class if they have won, otherwise return nil.
-	#
-	$counter=0
+
 	def winner
 		# Check each of the winning moves on the board, rows, cols and diagonals
 		# to see if a Player has filled a row of three consequtive squares	
-			@board.each_winning_move do |move|
-				if move[0] == @turn.marker && move[1] == @turn.marker && move[2] == @turn.marker
-						puts "#{@turn} won!"
-						Process.exit(0)
-				end	
-			end
-
-	 if $proceed		
-		 $counter+=1
-	 end
-		if $counter == 9
-			puts "Draw!"
-		    Process.exit(0)
+		@board.each_winning_move do |move|
+			if move[0] == @turn.marker && move[1] == @turn.marker && move[2] == @turn.marker
+				return @turn
+			end	
 		end
-		 #binding.pry
-
 	end
 
 	def user_choice 
-		while true
-			while true
-				x=""
-				y=""
-				puts "Please select which tile you want to target as x,y values"
-				input = gets.chomp.split(",")
-				x=input[0]
-				y=input[1]
-				if x==nil||y==nil
-					x,y=3
-				else
-					x.to_i
-					y.to_i
-				end
-				if x.to_i<3 && y.to_i<3 
-					@board.mark(x,y,@turn.marker)
-					draw
-					winner	
-					next_turn
-					break
+		puts "Please select which tile you want to target as x,y values"
+		input = gets.chomp.split(",")
 
-				else
-					draw
-
-				end	
-			end		 
-	     end
-	 end
+		if input.any?
+			yield input
+    end
+	end
 end
-
-def draw
-	puts @board.to_s	
-end
-
 
 class Player
 	def self.filled?(row)
